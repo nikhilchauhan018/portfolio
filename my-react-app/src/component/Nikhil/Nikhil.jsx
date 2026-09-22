4a1c4861c58d3b0c2caf378296654388f8b2b2c0
Nikhil Chauhan <nikhilkumarchauhan165@gmail.com>
2026-09-22 23:47:22 +0530

    Redesign

--- my-react-app/src/component/Nikhil/Nikhil.jsx
+++ my-react-app/src/component/Nikhil/Nikhil.jsx
@@ -10,6 +10,7 @@ const Nikhil = () => {
         <h1><span>I'M NIKHIL</span> frontend and backend developer from india</h1>
         <p>At GREENSORT IT, we deliver cutting-edge IT solutions designed to transform your business operations and drive success.
          Our comprehensive portfolio of services ensures that you stay ahead in the fast-paced digital world. Here’s what we offer:</p>
+        <p>Full stack developer </p>
          <div className="hero-action">
           <div className="hero-connect"> <AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with us</AnchorLink></div>
          </div>
